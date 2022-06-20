bf67cb8888dac2ff2dbe72865565f53f83beebc9
Connor119 <62894592+Connor119@users.noreply.github.com>
2022-06-20 20:04:20 +0800

    front end project

---
+++ jtUi/front-end/journeytime/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div>
+      <el-button type="primary" @click.native="hello">button</el-button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  },
+  methods:{
+    hello(){
+      console.log("hehehe")
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
