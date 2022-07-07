d7141b6802c4254dc28f0b3457f602aea5dff6ab
flintdk <92164637+flintdk@users.noreply.github.com>
2022-07-07 12:00:31 +0100

    22/07/07 TK; Some notes for collab

--- jtApi/jtUtils.py
+++ jtApi/jtUtils.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 import json
+from msilib.schema import File
 import os, sys
 from flask import Response
+from numpy import choose
 
 # According to the article here:
 #    -> https://towardsdatascience.com/simple-trick-to-work-with-relative-paths-in-python-c072cdc9acb9
@@ -93,12 +95,11 @@ def download_dataset_as_file(query, name):
                 json_list = []
                 row_count = 0
 
-            # buffer = ''.join( \
-            #     [json.dumps(tripsQuery.all()[idx].serialize(), indent=4) \
-            #         for idx in range(chunk_posn, (chunk_posn + rows_chunk), 1)] \
-            #         )
-
-            json_list.append( json.dumps(row.serialize(), indent=4) )
+            # We removed the indend of four spaces from our generated json. It does look
+            # prettier - but it increases file sizes by circa 13% (these files are already
+            # large)
+            #json_list.append( json.dumps(row.serialize(), indent=4) )
+            json_list.append( json.dumps(row.serialize()) )
             row_count += 1
         
         # At the end - always yield whatever remains in the buffer...
@@ -109,3 +110,20 @@ def download_dataset_as_file(query, name):
 
     return Response(generate(query, name), mimetype='application/json', \
         headers={'Content-disposition': 'attachment; filename=' + name + '.json'})
+
+
+#         popssible approaches to zip response...
+
+# From first principles:
+# ======================
+# https://stackoverflow.com/questions/44185486/generate-and-stream-compressed-file-with-flask
+# https://stackoverflow.com/questions/44185486/generate-and-stream-compressed-file-with-flask/44387566#44387566
+
+# Using randomer plugin:
+# ======================
+# https://tilns.herokuapp.com/posts/9b9c0d06d7-generate-and-stream-zipfiles-on-the-fly-with-flask
+
+# Record limit in the configuration File
+# Two links for each file - user can choose file or json (file is a csv)
+# BUT if the user chooses json for a HUGE data set.... inform the user and then send a json "sample"
+
