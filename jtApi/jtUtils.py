28fdb51f61272159054ff313fd2a941e818d7047
flintdk <92164637+flintdk@users.noreply.github.com>
2022-07-08 15:13:40 +0100

    22/07/08 TK; Complete and test .gz file downloads, direct from the db. MASSIVE size difference. Time to clean up API site.

--- jtApi/jtUtils.py
+++ jtApi/jtUtils.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
+from flask import Response, stream_with_context
 import json
-from msilib.schema import File
 import os, sys
-from flask import Response
-from numpy import choose
+import struct
+import time
+import zlib
+
 
 # According to the article here:
 #    -> https://towardsdatascience.com/simple-trick-to-work-with-relative-paths-in-python-c072cdc9acb9
@@ -30,6 +32,10 @@ def loadCredentials():
     file.close  # Can close the file now we have the data loaded...
     return credentials
 
+##########################################################################################
+#  Extracts (JSON, .CSV)
+##########################################################################################
+
 # 'Module Private' helper function for the various methods that download files...
 def _get_next_chunk_size(rows_remain):
     """Calculate the next chunk size for a data set
@@ -37,7 +43,6 @@ def _get_next_chunk_size(rows_remain):
     Based on the remaining rows and the paramaterised optimimum chunk size
     """
     CHUNK_SIZE = int(loadCredentials()['DOWNLOAD_CHUNK_SIZE'])
-    print("get_next_chunk_size: rows_remain is ", rows_remain)
     rows_chunk = 0
     if rows_remain > 0:
         if rows_remain >= CHUNK_SIZE:
@@ -47,14 +52,14 @@ def _get_next_chunk_size(rows_remain):
 
     return rows_chunk
 
-def download_query_results_as_csv(query, name):
+def query_results_as_compressed_csv(query, name):
     """
 
     Returns a CSV File with one row per record in the query result set.
     """
 
-    def get_buffer(row_list, first_chunk, query):
-        if first_chunk:
+    def get_chunk(row_list, first_chunk_tf, query):
+        if first_chunk_tf:
             # We need to build the string for the first line of the .csv - the column headers
             col_headers = ''
             for column_desc in query.statement.columns.keys():
@@ -63,73 +68,97 @@ def download_query_results_as_csv(query, name):
             if len(col_headers) > 1:
                 col_headers = col_headers[:-1]
 
-            buffer = col_headers + '\n'
-            first_chunk = False
+            chunk = col_headers + '\n'
+            first_chunk_tf = False
         else:
-            buffer = '\n'
-        buffer += '\n'.join(row_list)  # csv - join the elements with 'commas'
-        return buffer, first_chunk
+            chunk = '\n'
+        chunk += '\n'.join(row_list)  # csv - join the elements with 'commas'
+        return chunk.encode(), first_chunk_tf
     
-    def generate(query, name):
-        row_count = 0
+    def generate(query):
+        # I simply could not have implemented this method without:
+        #   -> https://stackoverflow.com/questions/44185486/generate-and-stream-compressed-file-with-flask
+        # ... as a reference.  I have not researched the Gzip file specification
+        # in detail (https://datatracker.ietf.org/doc/html/rfc1952#section-2.2),
+        # rather I've copied the values supplied by Martijn Pieters in the
+        # stackoverflow article above and adopted his approach to suit my needs.
 
-        # "tripsQuery.all()" is a python list of results
-        #rows_remain = len(tripsQuery.all())
-        rows_remain = query.count()
-        rows_chunk  = _get_next_chunk_size(rows_remain)
-        row_count   = 0
-        row_list   = []
-        first_chunk = True
+        # Yield a gzip file header first.
+        yield bytes([
+            # 0x1f0x8b is the Magic number identifying file type
+            # 0x08 is the compression method (deflate)
+            0x1F, 0x8B, 0x08, 0x00,  # Gzip file, deflate, no file flags
+            *struct.pack('<L', int(time.time())),  # compression start time
+            0x02, 0xFF,  # maximum compression, no OS specified
+        ])
+
+        # bookkeeping: the compression state, running CRC and total length
+        compressor = zlib.compressobj(
+            9, zlib.DEFLATED, -zlib.MAX_WBITS, zlib.DEF_MEM_LEVEL, 0)
+        crc = zlib.crc32(b"")
+
+        length = 0
+        rows_remain    = query.count()
+        rows_chunk     = _get_next_chunk_size(rows_remain)
+        row_count      = 0
+        row_list       = []
+        first_chunk_tf = True
         for row in query:
-            # Don't have to consider empty queries - if we have no results
-            # we will never enter this iterator.
-            
-            # Every time we complete a chunk we yield it and reset out list
-            # to an empty list...
-            if row_count > rows_chunk:
-                print("Starting next chunk...")
-                rows_remain -= rows_chunk  # we've jusr competed a chunk
-                rows_chunk = _get_next_chunk_size(rows_remain)
 
-                buffer, first_chunk = get_buffer(row_list, first_chunk, query)
-                if buffer:
-                    yield buffer
-                
-                row_list = []
-                row_count = 0
-
-            # If we build up our the string we plan to send by repeatedly appending,
-            # we're creating a new string each time.  This is quite memory expensive
+            # If we build up the string we plan to send by repeatedly appending,
+            # we're creating a new string each time. This is quite memory expensive
             # and inefficient.
-            # Instead we build a list of objects - which we will later convert to a string.
+            # Instead we build a list of strings - which we will later convert to
+            # a single string.
             # 'row_list' will be a list of strings...
             row_list.append( '"' + '","'.join([str(value) for value in row.serialize().values()]) + '"')
             row_count += 1
-        
-        # At the end - always yield whatever remains in the buffer...
-        # We always drop the last comma and close out our json list.
-        buffer, first_chunk = get_buffer(row_list, first_chunk, query)
-        if buffer:
-            yield buffer
 
-    return Response(generate(query, name), mimetype='text/csv', \
-        headers={'Content-disposition': 'attachment; filename=' + name + '.csv'})
+            if row_count >= rows_chunk:
+                rows_remain -= rows_chunk  # we've just competed a chunk
+                                           # this condition finally exits the loop
+                rows_chunk = _get_next_chunk_size(rows_remain)
+                # print("Completed a chunk:")
+                # print("\tRows Remaining ->", rows_remain)
+                # print("\tRows This Chunk ->", rows_chunk)
+
+                chunk, first_chunk_tf = get_chunk(row_list, first_chunk_tf, query)
+                chunk_compressed = compressor.compress(chunk)
+                if chunk_compressed:
+                    yield chunk_compressed
+                crc = zlib.crc32(chunk, crc) & 0xFFFFFFFF  # Keep the CRC up to date...
+                length += len(chunk)
+
+                row_list = []
+                row_count = 0
+
+        # Finishing off, send remainder of the compressed data, and CRC and length
+        yield compressor.flush()
+        yield struct.pack("<2L", crc, length & 0xFFFFFFFF)
+
+    # Test - do we need 'stream_with_context'?  If I understand the docs correctly
+    # then I don't think we do! It's just a method to keep the context alive while
+    # streaming).  But time/testing will tell...
+    #response = Response(stream_with_context(generate(query, name)), mimetype='application/gzip')
+    response = Response(generate(query), mimetype='application/gzip')
+    response.headers['Content-Disposition'] = 'attachment; filename=' + name + '.csv.gz'
+    return response
 
 
-def download_query_results_as_json(query, name):
+def query_results_as_json(query, name):
     """
 
     Returns a JSON List with one object per record in the query result set.
     """
 
-    def get_buffer(json_list, first_chunk, name):
+    def get_chunk(json_list, first_chunk, name):
         if first_chunk:
-            buffer = '{\n\"' + name + '\": [\n'
+            chunk = '{\n\"' + name + '\": [\n'
             first_chunk = False
         else:
-            buffer = ',\n'
-        buffer += ',\n'.join(json_list)
-        return buffer, first_chunk
+            chunk = ',\n'
+        chunk += ',\n'.join(json_list)
+        return chunk, first_chunk
     
     # See...
     #https://stackoverflow.com/questions/19926089/python-equivalent-of-java-stringbuffer
@@ -149,49 +178,32 @@ def download_query_results_as_json(query, name):
         for row in query:
             # Don't have to consider empty queries - if we have no results
             # we will never enter this iterator.
+
+            # We removed the indent of four spaces from our generated json. It does look
+            # prettier - but it increases download sizes by circa 13% (these extracts are
+            # already large)
+            #json_list.append( json.dumps(row.serialize(), indent=4) )
+            json_list.append( json.dumps(row.serialize()) )
+            row_count += 1
             
             # Every time we complete a chunk we yield it and reset out list
             # to an empty list...
-            if row_count > rows_chunk:
-                print("Starting next chunk...")
+            if row_count >= rows_chunk:
                 rows_remain -= rows_chunk  # we've jusr competed a chunk
                 rows_chunk = _get_next_chunk_size(rows_remain)
+                print("Completed a chunk:")
+                print("\tRows Remaining ->", rows_remain)
+                print("\tRows This Chunk ->", rows_chunk)
 
-                buffer, first_chunk = get_buffer(json_list, first_chunk, name)
+                buffer, first_chunk = get_chunk(json_list, first_chunk, name)
                 if buffer:
                     yield buffer
                 
                 json_list = []
                 row_count = 0
 
-            # We removed the indend of four spaces from our generated json. It does look
-            # prettier - but it increases file sizes by circa 13% (these files are already
-            # large)
-            #json_list.append( json.dumps(row.serialize(), indent=4) )
-            json_list.append( json.dumps(row.serialize()) )
-            row_count += 1
-        
-        # At the end - always yield whatever remains in the buffer...
-        # We always drop the last comma and close out our json list.
-        buffer, first_chunk = get_buffer(json_list, first_chunk, name)
-        if buffer:
-            yield buffer + "\n]\n}"  # <- We close the last after the last buffer yield
+        # Finishing off, send remainder of the compressed data, and CRC and length
+        yield "\n]\n}"  # <- We close the list after the last buffer yield
 
     return Response(generate(query, name), mimetype='application/json', \
         headers={'Content-disposition': 'attachment; filename=' + name + '.json'})
-
-#         popssible approaches to zip response...
-
-# From first principles:
-# ======================
-# https://stackoverflow.com/questions/44185486/generate-and-stream-compressed-file-with-flask
-# https://stackoverflow.com/questions/44185486/generate-and-stream-compressed-file-with-flask/44387566#44387566
-
-# Using randomer plugin:
-# ======================
-# https://tilns.herokuapp.com/posts/9b9c0d06d7-generate-and-stream-zipfiles-on-the-fly-with-flask
-
-# Record limit in the configuration File
-# Two links for each file - user can choose file or json (file is a csv)
-# BUT if the user chooses json for a HUGE data set.... inform the user and then send a json "sample"
-
