d1e99846c62da099a1566d64fa521af6c6c8a66a
Colump <92551741+Colump@users.noreply.github.com>
2022-07-27 16:15:28 +0100

    About Us content

--- jtUi/front-end/journeytime/src/components/AboutUs.vue
+++ jtUi/front-end/journeytime/src/components/AboutUs.vue
@@ -1,48 +1,33 @@
 <template>
-<div>
-       
+<div class="text">
+  <h1> About Us</h1>
+       <p>Journeyti.me is a free to use web application developed by 4 MSc Computer Science from University College Dublin. 
+         The application gives users a tool to recommend the best bus route or routes between a users desired start and end locations.
+         Our prediction engine takes into account a nnumber of factors such as historical bus journey data, weather conditions, traffic data and so on.
+      <br>
+        A unique selling point of our web application is the devlopmetn of our own API. While we acknowledge our API will not be as advanced as other APIs such as GTFS and GTFS-R, 
+        but we hope that it can be of use to other groups undertaking similar projects in the future. Our API can be accesed through the API page in our main navigation bar. 
+      <br>
+
+
+       </p>
 </div>
 </template>
 
 <script>
-export default {
-    name:'AboutUs',
-    data() {
-      return {
-        form: {
-          journeyFromGoogle: [],
-          show: false,
-        }
-      }
-    },
-    mounted(){
-        this.$bus.$on('GetAlljourney',(data)=>{
-        this.journeyFromGoogle = data
-        // console.log(data)
-      })
 
-    },
-    watch:{
-        journeyFromGoogle(){
-        }
-    },
-    methods:{
-        showAllRoute(){
-            this.form.show = true;
-            this.$bus.$on('GetAlljourney',(data)=>{
-                this.journeyFromGoogle = data
-            // console.log(data)
-            })
-
-        }
-
-    }
-
-
-
-}
 </script>
 
 <style>
-
+h1{
+  font-size: 40px;
+  color: rgb(0, 140, 255);
+}
+.text{
+  font-size: 20px;
+  text-align: center;
+  padding: 70px 0;
+  margin: auto;
+  width: 50%;
+}
 </style>
